16632780808067e40a0c6877c2f7703874b22d27
Chandan Kumar <chandankrr.91@gmail.com>
2024-05-03 11:39:16 +0530

    Design navbar

--- src/main/java/com/chandankrr/scm/controller/PageController.java
+++ src/main/java/com/chandankrr/scm/controller/PageController.java
@@ -13,4 +13,34 @@ public class PageController {
         log.info("Home page handler");
         return "home";
     }
+
+    @RequestMapping("/about")
+    public String about() {
+        log.info("About page handler");
+        return "about";
+    }
+
+    @RequestMapping("/services")
+    public String services() {
+        log.info("services page handler");
+        return "services";
+    }
+
+    @RequestMapping("/contact")
+    public String contact() {
+        log.info("contact page handler");
+        return "contact";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        log.info("login page handler");
+        return "login";
+    }
+
+    @RequestMapping("/signup")
+    public String signup() {
+        log.info("signup page handler");
+        return "signup";
+    }
 }
